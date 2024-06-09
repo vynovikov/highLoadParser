1808bcdb381aae868a751d1ddff608920a01b324
Vasiliy Novikov <v.y.novikov@gmail.com>
2024-06-09 20:46:29 +0300

    testing kafka in docker-compose

--- internal/transmitters/transmitters.go
+++ internal/transmitters/transmitters.go
@@ -2,7 +2,9 @@ package transmitters
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 	"github.com/vynovikov/highLoadParser/internal/encoder"
@@ -21,20 +23,56 @@ type transmittersStruct struct {
 
 func NewTransmitter(enc encoder.Encoder) *transmittersStruct {
 
-	broker := os.Getenv("KAFKA_ADDR")
-	topic := os.Getenv("KAFKA_TOPIC")
+	kafkaAddr := os.Getenv("KAFKA_ADDR")
+	kafkaPort := os.Getenv("KAFKA_PORT")
+	kafkaTopic := os.Getenv("KAFKA_TOPIC")
 
-	logger.L.Infof("in transmitters.NewTransmitter broker: %s, topic: %s\n", broker, topic)
+	logger.L.Infof("in transmitters.NewTransmitter broker: %s:%s, topic: %s\n", kafkaAddr, kafkaPort, kafkaTopic)
 
-	return &transmittersStruct{
+	var (
+		conn *kafka.Conn
+		err  error
+	)
 
-		saverKafkaWriter: kafka.NewWriter(kafka.WriterConfig{
-			Brokers:  []string{broker},
-			Topic:    topic,
-			Balancer: &kafka.RoundRobin{},
-		}),
-		encoder: enc,
+	for i := 0; i < 5; i++ {
+
+		logger.L.Infof("in transmitters.NewTransmitter %d attempt to connect to %s %s", i, "tcp", fmt.Sprintf("%s:%s", kafkaAddr, kafkaPort))
+
+		conn, err = kafka.Dial("tcp", fmt.Sprintf("%s:%s", kafkaAddr, kafkaPort))
+		if err != nil {
+			logger.L.Errorf("in rpc.NewReceiver cannot dial: %v\n", err)
+			time.Sleep(time.Second * 10)
+			if conn != nil {
+				conn.Close()
+			}
+		}
 	}
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+
+		logger.L.Errorln(err)
+		os.Exit(1)
+	}
+
+	for _, p := range partitions {
+
+		if p.Topic == kafkaTopic {
+
+			logger.L.Infof("in rpc.NewReceiver topic %s is found\n", kafkaTopic)
+
+			return &transmittersStruct{
+
+				saverKafkaWriter: kafka.NewWriter(kafka.WriterConfig{
+					Brokers:  []string{fmt.Sprintf("%s:%s", kafkaAddr, kafkaPort)},
+					Topic:    kafkaTopic,
+					Balancer: &kafka.RoundRobin{},
+				}),
+				encoder: enc,
+			}
+		}
+	}
+
+	return &transmittersStruct{}
 }
 
 func (t *transmittersStruct) TransmitToLogger(TransferUnit) error {
